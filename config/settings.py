c8be60b478ea6fd1528c34dc6a14f70c5fbf52ef
Arhum Savera <arhum.a.savera@gmail.com>
2021-10-21 23:48:52 +0500

    added API documentation

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     # 3rd party
     "rest_framework",
     "celery",
+    "drf_yasg",
     # local
     "theeye",
 ]
--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,31 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(  # new
+    openapi.Info(
+        title="TheEye API",
+        default_version="v1",
+        description="A sample API for ConsumerAffairs Event Analytics",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="hello@example.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include("theeye.urls")),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
